056a818abbe17b437aa549476d197a342bcd1613
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-18 14:58:02 +0530

    basics of tuple

---
+++ third/tuple.ts
@@ -0,0 +1,7 @@
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ["brown", true, 40];
+const sprite: Drink = ["white", true, 30];
+const coke: Drink = ["black", true, 65];
+
+console.log(pepsi);
