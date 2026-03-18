91c0cd92315d6a12d3544112af03787ebe8e4126
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-18 18:07:32 +0530

    understand of inheritance | modifiers

--- third/classes.ts
+++ third/classes.ts
@@ -3,12 +3,24 @@ class Vehicle {
     console.log("chugga chugga");
   }
 
-  honk(): void {
+  protected honk(): void {
     console.log("beep");
+  }
+
+  static blowHorn(): void {
+    console.log("Clear the road");
+  }
+}
+
+class Car extends Vehicle {
+  honk() {
+    console.log("Beep");
   }
 }
 
 const vehicle = new Vehicle();
+const car = new Car();
 
-vehicle.drive();
-vehicle.honk();
+Vehicle.blowHorn();
+car.drive();
+car.honk();
