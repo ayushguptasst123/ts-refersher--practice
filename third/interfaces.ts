7c814d17d4729cb81f73e87f495427576a6f227f
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-18 16:39:11 +0530

    inteface with incomplete data

--- third/interfaces.ts
+++ third/interfaces.ts
@@ -8,6 +8,15 @@ interface Human {
   gender: string;
   canEat: boolean;
 }
+/**
+ * It doesn't mean we have to add all the values to the interface.
+ * We can add values as per our needs.
+ */
+interface halfData {
+  name: string;
+  gender: string;
+  havePet: boolean;
+}
 
 const ramesh = {
   havePet: true,
@@ -15,18 +24,30 @@ const ramesh = {
   name: "Ramesh",
   gender: "male",
   canEat: true,
+  printData(): void {
+    console.log(`Name: ${this.name}`);
+    console.log(`Is he walking: ${this.isWalk}`);
+    console.log(`will ${this.name} have pet?  ${this.havePet}`);
+    console.log(`Gender: ${this.gender}`);
+    console.log(`Eating? ${this.canEat}`);
+  },
 };
 
 /**
  * @param profile
  * Instead of writing all the annotations, we can create an interface and put all the variables there.
  */
-const printProfile = (profile: Human) => {
-  console.log(`Name: ${profile.name}`);
-  console.log(`Is he walking: ${profile.isWalk}`);
-  console.log(`will ${profile.name} have pet?  ${profile.havePet}`);
-  console.log(`Gender: ${profile.gender}`);
-  console.log(`Eating? ${profile.canEat}`);
+const printProfile = (profile: { printData(): void }) => {
+  profile.printData();
 };
 
+const printHalfData = (data: halfData) => {
+  console.log(
+    `${data.name} is ${data.gender} and it Like pet? ${data.havePet}`,
+  );
+};
+
+printHalfData(ramesh);
+console.log("*".repeat(30));
+console.log();
 printProfile(ramesh);
