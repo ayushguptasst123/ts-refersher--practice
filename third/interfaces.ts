3863c15fc9b0a9db8e0b3b88be517d0e89da77d8
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-18 15:34:13 +0530

    using real usecase of interface

--- third/interfaces.ts
+++ third/interfaces.ts
@@ -1,17 +1,32 @@
-const oldCivic = {
-  name: "civic",
-  year: 2000,
-  broken: true,
-};
-
-const printVehicle = (vehicle: {
+/**
+ * The interface keys must be the same as those of the reference object.
+ */
+interface Human {
+  havePet: boolean;
+  isWalk: boolean;
   name: string;
-  year: number;
-  broken: boolean;
-}): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+  gender: string;
+  canEat: boolean;
+}
+
+const ramesh = {
+  havePet: true,
+  isWalk: true,
+  name: "Ramesh",
+  gender: "male",
+  canEat: true,
 };
 
-printVehicle(oldCivic);
+/**
+ * @param profile
+ * Instead of writing all the annotations, we can create an interface and put all the variables there.
+ */
+const printProfile = (profile: Human) => {
+  console.log(`Name: ${profile.name}`);
+  console.log(`Is he walking: ${profile.isWalk}`);
+  console.log(`will ${profile.name} have pet?  ${profile.havePet}`);
+  console.log(`Gender: ${profile.gender}`);
+  console.log(`Eating? ${profile.canEat}`);
+};
+
+printProfile(ramesh);
