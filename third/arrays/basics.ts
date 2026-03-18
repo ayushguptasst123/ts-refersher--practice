a351865954ecc57467f45bb722699b1c0fd1a764
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-18 13:30:10 +0530

    object inside array

--- third/arrays/basics.ts
+++ third/arrays/basics.ts
@@ -6,8 +6,33 @@ const carByMake: (string[] | number[])[] = [
   [1999, 1803, 1834],
 ];
 
-
 // carMakers.push(34); //We can't do this
 carMakers.push("audi");
 
-console.log(carMakers.map(car => car.toUpperCase()));
+console.log(carMakers.map((car) => car.toUpperCase()));
+
+const dummyArr: {
+  name: string;
+  role: string;
+  bounty: number;
+  saySomething(): void;
+}[] = [
+  {
+    name: "luffy",
+    role: "captain",
+    bounty: 1500000,
+    saySomething(): void {
+      console.log("I will be the king of pirate");
+    },
+  },
+  {
+    name: "zoro",
+    role: "swordsMan",
+    bounty: 100000,
+    saySomething(): void {
+      console.log("My Name goes to heaven ");
+    },
+  },
+];
+
+dummyArr[1].saySomething();
