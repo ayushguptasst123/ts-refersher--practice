8a69ed0d56686487af0665a2a860a8fb8ce6b563
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-19 17:53:16 +0530

    orderData class DONE

---
+++ oopsProject/items/orderData.ts
@@ -0,0 +1,75 @@
+/**
+ * Menu--1
+ */
+export const orderItem1 = [
+  { name: "Pizza", price: 250 },
+  { name: "Burger", price: 120 },
+];
+export const orderItem2 = [
+  { name: "Pasta", price: 200 },
+  { name: "Sandwich", price: 150 },
+  { name: "Fries", price: 90 },
+];
+export const orderItem3 = [
+  { name: "Pizza", price: 250 },
+  { name: "Burger", price: 120 },
+  { name: "Pasta", price: 200 },
+  { name: "Sandwich", price: 150 },
+  { name: "Fries", price: 90 },
+  { name: "Wrap", price: 180 },
+];
+export const orderItem4 = [
+  { name: "Wrap", price: 180 },
+  { name: "Fries", price: 90 },
+];
+
+/**
+ * Menu--2
+ */
+export const orderItem5 = [
+  { name: "Coffee", price: 80 },
+  { name: "Tea", price: 50 },
+];
+export const orderItem6 = [
+  { name: "Cold Drink", price: 100 },
+  { name: "Lemonade", price: 70 },
+  { name: "Mojito", price: 120 },
+];
+export const orderItem7 = [
+  { name: "Coffee", price: 80 },
+  { name: "Tea", price: 50 },
+  { name: "Cold Drink", price: 100 },
+  { name: "Lemonade", price: 70 },
+  { name: "Milkshake", price: 130 },
+  { name: "Mojito", price: 120 },
+];
+
+export const orderItem8 = [
+  { name: "Milkshake", price: 130 },
+  { name: "Mojito", price: 120 },
+];
+
+/**
+ * Menu-3
+ */
+export const orderItem9 = [
+  { name: "Ice Cream", price: 90 },
+  { name: "Cake", price: 150 },
+];
+export const orderItem10 = [
+  { name: "Brownie", price: 130 },
+  { name: "Donut", price: 60 },
+  { name: "Cupcake", price: 80 },
+];
+export const orderItem11 = [
+  { name: "Ice Cream", price: 90 },
+  { name: "Cake", price: 150 },
+  { name: "Brownie", price: 130 },
+  { name: "Donut", price: 60 },
+  { name: "Cupcake", price: 80 },
+  { name: "Waffle", price: 140 },
+];
+export const orderItem12 = [
+  { name: "Waffle", price: 140 },
+  { name: "Brownie", price: 130 },
+];
